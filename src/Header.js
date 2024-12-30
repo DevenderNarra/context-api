cba37f508c9ec64c0cb0485052e49bfec13d6f02
DevenderNarra <devendernarra@gmail.com>
2024-12-30 17:20:50 +0530

    items

--- src/Header.js
+++ src/Header.js
@@ -1,13 +1,15 @@
 import {Link} from 'react-router-dom'
-import React from 'react'
+import React, { useContext } from 'react'
+import { cartContext } from './Context'
 
 const Header = () => {
+  const {cart}=useContext(cartContext)
   return (
     <div className='header'>
      <h1>Context-api</h1>
      <ul className='menu'>
       <Link to="/"><span>Homepage</span></Link>
-      <Link to="/cart"><span>Cart</span></Link>
+      <Link to="/cart"><span>Cart:{cart.length}</span></Link>
      </ul>
     </div>
   )
